20c376440d2fe1508f79197c948ce1da4d8e2258
Raúl Zavaleta <raul.zavaletazea@gmail.com>
2021-06-21 19:52:37 -0500

    Screen State y Menu

--- demo-firebase/src/screens/EjemploState.js
+++ demo-firebase/src/screens/EjemploState.js
@@ -1,18 +1,49 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 
 const EjemploState = (props) => {
-    return (
-        <View style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            flex: 1
-        }}>
-            <Text>
-                En EjemploState.js
-            </Text>
-        </View>
-    );
-}
- 
+	return (
+		<View
+			style={{
+				flex: 1,
+			}}
+		>
+			<View
+				style={{
+					flex: 1,
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<Button title='+' color='tomato' />
+			</View>
+			<View
+				style={{
+					flex: 1,
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<Text
+					style={{
+						color: 'tomato',
+						fontSize: 128,
+					}}
+				>
+					0
+				</Text>
+			</View>
+			<View
+				style={{
+					flex: 1,
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<Button title='-' color='tomato' />
+			</View>
+		</View>
+	);
+};
+
 export default EjemploState;
--- demo-firebase/src/screens/Menu.js
+++ demo-firebase/src/screens/Menu.js
@@ -1,30 +1,46 @@
 import React from 'react';
-import { ScrollView, View, Text, Button } from 'react-native';
+import {
+	ScrollView,
+	View,
+	Text,
+	Button,
+} from 'react-native';
 
 const Menu = (props) => {
-    return (
-        <ScrollView>
-            <Button
-                title='Ejemplo Flex'
-                color='tomato'
-                /**
-                 * OnPress utiliza una función 
-                 * (Función Flecha)
-                 * anónimas para indicar las 
-                 * acciones del evento
-                 * 
-                 * Los hijos de un NAvigationContainer tienen
-                 * una prop llamada navigation con la que pueden
-                 * utilizar, modificar o eliminar la navegación
-                 * de la app
-                 * 
-                 * navigation.navigate('_NOMBRE_SCREEN')
-                 */
-                onPress={
-                    () => props.navigation.navigate('EjemploFlex')
-                } />
-        </ScrollView>
-    );
-}
- 
+	return (
+		<ScrollView>
+			<Button
+				title='Ejemplo Flex'
+				color='tomato'
+				/**
+				 * OnPress utiliza una función
+				 * (Función Flecha)
+				 * anónimas para indicar las
+				 * acciones del evento
+				 *
+				 * Los hijos de un NAvigationContainer tienen
+				 * una prop llamada navigation con la que pueden
+				 * utilizar, modificar o eliminar la navegación
+				 * de la app
+				 *
+				 * navigation.navigate('_NOMBRE_SCREEN')
+				 */
+				onPress={() =>
+					props.navigation.navigate('EjemploFlex')
+				}
+			/>
+
+			<Button
+				title='Ejemplo State'
+				color='tomato'
+				onPress={() =>
+					props.navigation.navigate(
+						'EjemploState'
+					)
+				}
+			/>
+		</ScrollView>
+	);
+};
+
 export default Menu;
